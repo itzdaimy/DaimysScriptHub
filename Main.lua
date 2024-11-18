6fdfba8eb28dbec0b381941fbed67635cae36cf4
itzdaimy <itzdaimy@gmail.com>
2024-11-18 20:47:08 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,4 +1,4 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/itzdaimy/DaimysScriptHub/refs/heads/main/UIlibrary'))()
 local gameid = game.GameId
 local placeid = game.PlaceId
 
